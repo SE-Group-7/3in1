6ce47468e8c5c428740b24341fb0bdc866c55453
tri-ler <tylerh689@gmail.com>
2020-04-26 16:26:08 -0400

    added analytics section to tables

--- Components/tablepage1.js
+++ Components/tablepage1.js
@@ -1,8 +1,11 @@
 import React, { useState} from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function Menu({navigation}){
+
   return (
+    <ScrollView>
     <View style={{ flex: 1, alignItems: 'center'}}>
       <Text style={styles.title}>Bill</Text>
       <Text style={styles.menuItem}> Hamburger: $12.00</Text>
@@ -21,8 +24,30 @@ export default function Menu({navigation}){
       <Text stlyle={styles.menuItem}>Server Sam recieved order</Text>
       <Text stlyle={styles.menuItem}>Guests order sent to chefs</Text>
       <Text stlyle={styles.menuItem}>Guests order placed</Text>
-      <Text stlyle={styles.menuItem}>Guests sat by server</Text>
+      <Text stlyle={styles.menuItem}>4 Guests sat by server</Text>
+      <Text style={styles.title}>Data</Text>
+      <Text stlyle={styles.menuItem}>Average server time: 5:32</Text>
+      <Button
+        title="Refresh"
+        onPress={() => console.log("refreshing statistic") }
+      ></Button>
+      <Text stlyle={styles.menuItem}>Average wait time: 3:32</Text>
+      <Button
+        title="Refresh"
+        onPress={() => console.log("refreshing statistic") }
+      ></Button>
+      <Text stlyle={styles.menuItem}>Average number of guests: 3</Text>
+      <Button
+        title="Refresh"
+        onPress={() => console.log("refreshing statistic") }
+      ></Button>
+      <Text stlyle={styles.menuItem}>Percent Time Occupied: 45%</Text>
+      <Button
+        title="Refresh"
+        onPress={() => console.log("refreshing statistic") }
+      ></Button>
     </View>
+    </ScrollView>
   );
 }
 
